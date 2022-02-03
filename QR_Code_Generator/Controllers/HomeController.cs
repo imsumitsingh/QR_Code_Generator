60eb1741f82da3e918080290fc168b00b03104e6
Sumit Kumar <imsumitsingh_@outlook.com>
2022-02-03 23:46:28 +0530

    Add project files.

---
+++ QR_Code_Generator/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QR_Code_Generator.Models;
+using QRCoder;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace QR_Code_Generator.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+
+
+        public IActionResult Index()
+        {
+            string code = "sumit";
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(code, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+
+            using (Bitmap bitMap = qrCode.GetGraphic(20))
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    bitMap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                    byte[] byteImage = ms.ToArray();
+                    ViewBag.url = "data:image/png;base64," + Convert.ToBase64String(byteImage);
+                }
+              ;
+
+                return View();
+            }
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Qr(string str)
+        {
+            string code = str is null || str==""?"Found Nothing":str;
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(code, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+
+            using (Bitmap bitMap = qrCode.GetGraphic(20))
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    bitMap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                    byte[] byteImage = ms.ToArray();
+                    ViewBag.url = "data:image/png;base64," + Convert.ToBase64String(byteImage);
+                }
+              ;
+
+                return PartialView("QrCode");
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ QR_Code_Generator/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddRazorPages().AddViewOptions(options =>
+{
+    options.HtmlHelperOptions.ClientValidationEnabled = true;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
